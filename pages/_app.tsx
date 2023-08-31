db954f9aade88e6a08e35310a1f3343669982d6c
Ntwali Edson <ntwaliedson9@gmail.com>
2023-08-31 11:00:05 +0200

    feat: removed unsed packages.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,9 @@
 import { AppProps } from "next/app";
 import "../styles/globals.css";
-import { QueryClient, QueryClientProvider } from "react-query";
 import { Toaster } from "react-hot-toast";
 import { Providers } from "@/components/providers";
 import { cn } from "@/lib/utils";
 import { fontMono, fontSans } from "@/lib/fonts";
-const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -16,16 +14,14 @@ export default function App({ Component, pageProps }: AppProps) {
         fontMono.variable
       )}
     >
-      <QueryClientProvider client={queryClient}>
-        <Toaster />
-        <Providers attribute="class" defaultTheme="system" enableSystem>
-          <div className="flex flex-col min-h-screen">
-            <main className=" bg-muted/50">
-              <Component {...pageProps} />
-            </main>
-          </div>
-        </Providers>
-      </QueryClientProvider>
+      <Toaster />
+      <Providers attribute="class" defaultTheme="system" enableSystem>
+        <div className="flex flex-col min-h-screen">
+          <main className=" bg-muted/50">
+            <Component {...pageProps} />
+          </main>
+        </div>
+      </Providers>
     </main>
   );
 }
